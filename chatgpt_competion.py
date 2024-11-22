b3f4c0959a1d54501c0d6eb13b808dc215115f9b
Yuval Maliniak <yuvalmali5@gmail.com>
2024-11-22 16:51:24 +0200

    Writing to file works, added also example so to check it the file exec works

--- chatgpt_competion.py
+++ chatgpt_competion.py
@@ -8,9 +8,11 @@ completion = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
         {"role": "system", "content": "You are a helpful assistant for code completion. Write only code."},
-        {"role": "user", "content": "“Create a python program that checks if a number is even and divided by 9. Do not write any explanations, just show me the code itself.do not add examples just the function."},
+        {"role": "user", "content": "“Create a python program that checks if a number is even and divided by 9. Do not write any explanations, just show me the code itself."},
     ]
 )
 code_content = completion.choices[0].message.content
 clean_code = code_content.strip("```").replace("python", "").strip()
 print(clean_code)
+with open("generatedcode.py", "w") as file:
+    file.write(clean_code)
